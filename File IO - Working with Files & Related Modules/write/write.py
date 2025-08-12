6b78e2a4dc6989783578f27b7bc6ee09d958a626
Vidya Vardhana <252004poorvik@gmail.com>
2025-08-12 23:45:47 +0530

    Write-file

---
+++ File IO - Working with Files & Related Modules/write/write.py
@@ -0,0 +1,9 @@
+f = open("vv.txt", "w")
+
+string = '''Passionate about technology, I focus on AI, machine learning, and user-centered design. 
+Skilled in Python, C++, and web development, I've built intelligent automation projects. 
+I thrive on challenges, collaboration, and continuous learning, seeking to deliver impactful IT solutions
+'''
+
+f.write(string)
+f.close()   
