5ee9563f68b28fed2660f7e7cbd2f4ad135893ba
Vidya Vardhana <252004poorvik@gmail.com>
2025-08-12 23:55:13 +0530

    Append-file

---
+++ File IO - Working with Files & Related Modules/append/append.py
@@ -0,0 +1,9 @@
+f = open("vv.txt", "a")
+
+string = '''In addition to my technical skills, I am committed to lifelong learning and staying updated with the latest industry trends. 
+I enjoy working in dynamic environments where I can apply my knowledge to solve real-world problems and contribute to innovative projects.
+I believe in the power of teamwork and effective communication to achieve common goals, and I am always eager to share my insights and learn from others.
+'''
+
+f.write(string)
+f.close()   
